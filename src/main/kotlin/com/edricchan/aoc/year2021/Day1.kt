ad2547703c0c58077bd048a4210705c479926d4b
Edric Chan <edric.chan.1997@gmail.com>
2021-12-02 00:10:39 +0800

    Add 2021 Day 1 source code

--- src/main/kotlin/com/edricchan/aoc/year2021/Day1.kt
+++ src/main/kotlin/com/edricchan/aoc/year2021/Day1.kt
@@ -8,11 +8,32 @@ fun main() = solve { Day1() }
 class Day1 : Puzzle<Int, Int>(year = 2021, day = 1) {
     private val records = input.map { it.toInt() }
     override fun solvePartOne(): Int {
-        TODO("Not yet implemented")
+        var prevHighest = records.first()
+        var increaseTimes = 0
+
+        records.slice(1..records.lastIndex).forEach {
+            // println("Received number $it, previous highest number = $highestNum," +
+            //    " current no of times increased = $increaseTimes")
+            if (it > prevHighest) {
+                // Current record has increased
+                increaseTimes++
+            }
+            // Reset highest number
+            prevHighest = it
+        }
+
+        return increaseTimes
     }
 
     override fun solvePartTwo(): Int {
-        TODO("Not yet implemented")
+        // Can be done with Kotlin's Collections API
+        // windowed(3): [1,2,3,4,5,3] -> [[1,2,3], [2,3,4], [3,4,5], [4,5,3]]
+        // windowed(3) { it.sum() }: [[1,2,3], [2,3,4], [3,4,5], [4,5,3]] -> [6,9,12,12]
+        return records.windowed(3) { it.sum() }
+            // zipWithNext: [6,9,12,12] -> [6 to 9, 9 to 12, 12 to 12]
+            .zipWithNext()
+            // count: [6 to 9, 9 to 12, 12 to 15] -> 9 > 6? yes, 12 > 9? yes, 12 > 12? no
+            // two occurrences
+            .count { (prev, next) -> next > prev }
     }
-
 }
