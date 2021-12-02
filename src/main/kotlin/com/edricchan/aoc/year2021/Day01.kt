82552a957a697073a5c17ac933750e35c919ea9d
Edric Chan <edric.chan.1997@gmail.com>
2021-12-02 10:53:26 +0800

    2021/Day1.kt: Rename to Day01 to be more consistent when sorting file names

--- src/main/kotlin/com/edricchan/aoc/year2021/Day01.kt
+++ src/main/kotlin/com/edricchan/aoc/year2021/Day01.kt
@@ -3,9 +3,9 @@ package com.edricchan.aoc.year2021
 import com.edricchan.aoc.Puzzle
 import com.edricchan.aoc.solve
 
-fun main() = solve { Day1() }
+fun main() = solve { Day01() }
 
-class Day1 : Puzzle<Int, Int>(year = 2021, day = 1) {
+class Day01 : Puzzle<Int, Int>(year = 2021, day = 1) {
     private val records = input.map { it.toInt() }
     override fun solvePartOne(): Int {
         var prevHighest = records.first()
