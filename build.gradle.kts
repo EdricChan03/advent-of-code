723ba847d9b2e4c9b66aede0099f9593e992b24e
Edric Chan <edric.chan.1997@gmail.com>
2021-12-02 22:11:34 +0800

    Solver.kt: Add initial tests
    The getInput test eventually results in an OutOfMemory error so this has been temporarily disabled for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,14 @@ repositories {
 }
 
 dependencies {
+    // Testing frameworks
     testImplementation(kotlin("test"))
+    // Kotest
+    testImplementation(platform("io.kotest:kotest-bom:5.0.1"))
+    testImplementation("io.kotest:kotest-runner-junit5")
+    testImplementation("io.kotest:kotest-property")
+    // Mockk
+    testImplementation("io.mockk:mockk:1.12.1")
 }
 
 java.toolchain {
---
+++ src/test/kotlin/com/edricchan/aoc/year2021/SolverTest.kt
@@ -0,0 +1,94 @@
+package com.edricchan.aoc.year2021
+
+import com.edricchan.aoc.Puzzle
+import com.edricchan.aoc.getInput
+import com.edricchan.aoc.printResult
+import com.edricchan.aoc.solve
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.string
+import io.kotest.property.checkAll
+import io.mockk.*
+import java.io.BufferedReader
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.PrintStream
+
+class SolverTest : DescribeSpec({
+    // Override the default System.out/System.err
+    val defaultOutPrintStream = System.out
+    val testOutContentStream = ByteArrayOutputStream()
+    val testOutPrintStream = PrintStream(testOutContentStream)
+
+    val defaultErrStream = System.err
+    val testErrContentStream = ByteArrayOutputStream()
+    val testErrPrintStream = PrintStream(testErrContentStream)
+
+    beforeTest {
+        System.setOut(testOutPrintStream)
+        System.setErr(testErrPrintStream)
+    }
+
+    afterTest {
+        System.setOut(defaultOutPrintStream)
+        System.setErr(defaultErrStream)
+    }
+
+    // Test puzzle data
+    val (partOne, partTwo, year, day) = Arb.int().samples()
+        .take(4).map { it.value }.toList()
+
+    class TestPuzzle : Puzzle<Int, Int>(year = year, day = day) {
+        override fun solvePartOne(): Int {
+            return partOne
+        }
+
+        override fun solvePartTwo(): Int {
+            return partTwo
+        }
+    }
+
+    // TODO: Fix test
+    xdescribe("getInput") {
+        it("should retrieve the file data") {
+            val bufferedReader = mockk<BufferedReader>()
+
+            checkAll(10, Arb.int(2000..2040), Arb.int(1..31)) { year, day ->
+                val fileData = Arb.string().samples().take(3).map { it.value }.toList()
+
+                every {
+                    bufferedReader.readLines()
+                } returns fileData
+
+                getInput(year, day) shouldBe fileData
+            }
+        }
+    }
+
+    describe("printResult") {
+        it("should print the result of a puzzle") {
+            printResult { TestPuzzle() }
+
+            testOutContentStream.toString().trim().replace("\r\n", "\n") shouldBe """
+            $day December $year:
+            Part 1 result: ${TestPuzzle().solvePartOne()}
+            Part 2 result: ${TestPuzzle().solvePartTwo()}
+            """.trimIndent()
+        }
+    }
+
+    describe("solve") {
+        it("should call printResult") {
+            mockkStatic(::solve)
+
+            val puzzleFn: () -> Puzzle<*, *> = { TestPuzzle() }
+            val benchmark = true
+            solve(benchmark, puzzleFn)
+
+            verify { solve(benchmark, puzzleFn) }
+            verify { printResult(puzzleFn) }
+        }
+    }
+})
