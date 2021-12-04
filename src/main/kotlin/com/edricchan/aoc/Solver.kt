88772d436bfa886ca29e185531d26dd865230615
Edric Chan <edric.chan.1997@gmail.com>
2021-12-04 16:40:18 +0800

    Solver.kt: Explicitly throw error if input file could not be found
    This prevents issues such as the input text file not existing, resulting in the input data appearing to be empty

--- src/main/kotlin/com/edricchan/aoc/Solver.kt
+++ src/main/kotlin/com/edricchan/aoc/Solver.kt
@@ -1,15 +1,17 @@
 package com.edricchan.aoc
 
+import java.io.FileNotFoundException
+
 /**
  * Retrieves the input text file for a given [day] and [year].
  * @param year The year of the puzzle.
  * @param day The day of the puzzle.
  */
 fun getInput(year: Int, day: Int, fileName: String = "input.txt") =
-    Puzzle::class.java.classLoader.getResourceAsStream("aoc/year$year/day$day/$fileName")
-        ?.bufferedReader()
-        ?.readLines()
-        .orEmpty()
+    (Puzzle::class.java.classLoader.getResourceAsStream("aoc/year$year/day$day/$fileName")
+        ?: throw FileNotFoundException("Input file for $year, day $day does not exist"))
+        .bufferedReader()
+        .readLines()
 
 /**
  * Solves the specified puzzle.
@@ -17,7 +19,7 @@ fun getInput(year: Int, day: Int, fileName: String = "input.txt") =
  * @param block The higher order function to be used to retrieve the [Puzzle].
  */
 // TODO: Add benchmarking support
-fun solve(benchmark: Boolean = true, block: () -> Puzzle<*,*>) {
+fun solve(benchmark: Boolean = true, block: () -> Puzzle<*, *>) {
     printResult(block)
 }
 
