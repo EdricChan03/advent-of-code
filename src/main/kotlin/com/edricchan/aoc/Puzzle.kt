ef6bed0a06f2e2c04ab97008b4157987adab85d6
Edric <edric.chan.1997@gmail.com>
2022-12-06 14:33:34 +0800

    Puzzle: Add constructor arg to specify a custom input

--- src/main/kotlin/com/edricchan/aoc/Puzzle.kt
+++ src/main/kotlin/com/edricchan/aoc/Puzzle.kt
@@ -4,17 +4,20 @@ package com.edricchan.aoc
  * The puzzle question for a specific day.
  * @property year The specific year
  * @property day The specific day
+ * @property customInput The custom input, if any. If specified, this value
+ * takes precedence over the [input] property.
  * @property PartOneOutput The output type to be used for part 1
  * @property PartTwoOutput The output type to be used for part 2
  */
 abstract class Puzzle<PartOneOutput, PartTwoOutput>(
-    val year: Int = Int.MIN_VALUE, val day: Int = Int.MIN_VALUE
+    val year: Int = Int.MIN_VALUE, val day: Int = Int.MIN_VALUE,
+    private val customInput: String? = null
 ) {
     /** The input [File] for the specific puzzle. */
     val inputFile by lazy { getInputFile(year, day) }
 
     /** The input for the specific puzzle as a [List] of strings. */
-    val input by lazy { getInput(year, day) }
+    val input by lazy { customInput?.lines() ?: getInput(year, day) }
 
     /**
      * Allows for the input to be retrieved as a [Sequence] via [block],
@@ -26,7 +29,7 @@ abstract class Puzzle<PartOneOutput, PartTwoOutput>(
     fun <T> useInput(block: (Sequence<String>) -> T) = useInput(year, day, block = block)
 
     /** The input for the specific puzzle as a [String]. */
-    val rawInput by lazy { getRawInput(year, day) }
+    val rawInput by lazy { customInput ?: getRawInput(year, day) }
 
     /** Retrieves the solution for part 1 of type [PartOneOutput]. */
     abstract fun solvePartOne(): PartOneOutput
