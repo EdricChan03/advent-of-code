93d115689b0c60537d97c70c4bac6d6d317ff742
Edric <edric.chan.1997@gmail.com>
2024-01-20 01:36:56 +0800

    Solver: Add `PuzzleMeta` overloads for top-level functions
    Note that we can't pass it as a parameter as it would result in overload
    resolution ambiguity when used as a function reference.
    
    This commit adds support for using a `PuzzleMeta` for the following
    methods:
    
    * `getInputFilePath`
    * `getInputFile`
    * `getInputPath`
    * `getInput`
    * `useInput`
    * `getRawInput`

--- src/main/kotlin/com/edricchan/aoc/Solver.kt
+++ src/main/kotlin/com/edricchan/aoc/Solver.kt
@@ -63,6 +63,21 @@ fun getInputFilePath(
 ) = "aoc/year$year/day$day/$fileName"
 
 /**
+ * Retrieves the file path for the given [meta], optionally allowing
+ * for the [fileName] to be specified.
+ *
+ * This is equivalent to the string `"aoc/year${meta.year}/day${meta.day}/$fileName"`.
+ *
+ * To get the input as a [File], use [getInputFile].
+ * To get the input as a [Path], use [getInputPath].
+ * @receiver The puzzle's metadata, consisting of [PuzzleMeta.year] and [PuzzleMeta.day].
+ * @param fileName The text file's name.
+ */
+fun PuzzleMeta.getInputFilePath(
+    fileName: String = "input.txt"
+) = getInputFilePath(year = year.value, day = day, fileName = fileName)
+
+/**
  * Retrieves the input text file for a given [day] and [year] as a [File].
  * @param year The year of the puzzle.
  * @param day The day of the puzzle.
@@ -77,6 +92,22 @@ fun getInputFile(
     resourceLoader: ResourceLoader = ResourceLoader.Default
 ) = resourceLoader.getResource(getInputFilePath(year, day, fileName))
     ?: throw FileNotFoundException("Input file for year $year, day $day does not exist")
+
+/**
+ * Retrieves the input text file for a given [meta] as a [File].
+ * @receiver The puzzle metadata, consisting of [PuzzleMeta.year] and [PuzzleMeta.day].
+ * @param fileName The text file's name.
+ * @param resourceLoader The resource loader to be used to retrieve the input text file.
+ * @return The input text file as a [File] reference.
+ * @see getInputPath
+ * @see getInputFilePath
+ */
+fun PuzzleMeta.getInputFile(
+    fileName: String = "input.txt",
+    resourceLoader: ResourceLoader = ResourceLoader.Default
+) = getInputFile(
+    year = year.value, day = day, fileName = fileName, resourceLoader = resourceLoader
+)
 
 /**
  * Retrieves the input text file for a given [day] and [year] as a [Path].
@@ -95,6 +126,22 @@ fun getInputPath(
     ?: throw FileNotFoundException("Input file for year $year, day $day does not exist")
 
 /**
+ * Retrieves the input text file for a given [meta] as a [Path].
+ * @receiver The puzzle's metadata, consisting of [PuzzleMeta.year] and [PuzzleMeta.day].
+ * @param fileName The text file's name.
+ * @param resourceLoader The resource loader to be used to retrieve the input text file.
+ * @return The input text file as a [Path] reference.
+ * @see getInputFile
+ * @see getInputFilePath
+ */
+fun PuzzleMeta.getInputPath(
+    fileName: String = "input.txt",
+    resourceLoader: ResourceLoader = ResourceLoader.Default
+) = getInputPath(
+    year = year.value, day = day, fileName = fileName, resourceLoader = resourceLoader
+)
+
+/**
  * Retrieves the input text file's contents for a given [day] and [year].
  * @param year The year of the puzzle.
  * @param day The day of the puzzle.
@@ -106,6 +153,20 @@ fun getInput(
     year: Int, day: Int, fileName: String = "input.txt",
     resourceLoader: ResourceLoader = ResourceLoader.Default
 ) = getInputPath(year, day, fileName, resourceLoader).readLines()
+
+/**
+ * Retrieves the input text file's contents for a given [meta].
+ * @receiver The puzzle metadata, consisting of [PuzzleMeta.year] and [PuzzleMeta.day].
+ * @param fileName The text file's name.
+ * @param resourceLoader The resource loader to be used to retrieve the input text file.
+ * @return The contents of the text file as a list split by new lines.
+ */
+fun PuzzleMeta.getInput(
+    fileName: String = "input.txt",
+    resourceLoader: ResourceLoader = ResourceLoader.Default
+) = getInput(
+    year = year.value, day = day, fileName = fileName, resourceLoader = resourceLoader
+)
 
 /**
  * Retrieves the input text file's contents for a given [day] and [year] as a [Sequence].
@@ -123,6 +184,23 @@ fun <T> useInput(
 ) = getInputPath(year, day, fileName, resourceLoader).useLines(block = block)
 
 /**
+ * Retrieves the input text file's contents for a given [meta] as a [Sequence].
+ * @receiver The puzzle metadata, consisting of [PuzzleMeta.year] and [PuzzleMeta.day].
+ * @param fileName The text file's name.
+ * @param resourceLoader The resource loader to be used to retrieve the input text file.
+ * @param block A callback where the contents of the file are passed as a [Sequence].
+ * @return The result of the [block].
+ */
+fun <T> PuzzleMeta.useInput(
+    fileName: String = "input.txt",
+    resourceLoader: ResourceLoader = ResourceLoader.Default,
+    block: (Sequence<String>) -> T
+) = useInput(
+    year = year.value, day = day, fileName = fileName, resourceLoader = resourceLoader,
+    block = block
+)
+
+/**
  * Retrieves the input text file's contents for a given [day] and [year].
  * @param year The year of the puzzle.
  * @param day The day of the puzzle.
@@ -134,6 +212,20 @@ fun getRawInput(
     year: Int, day: Int, fileName: String = "input.txt",
     resourceLoader: ResourceLoader = ResourceLoader.Default
 ) = getInputPath(year, day, fileName, resourceLoader).readText()
+
+/**
+ * Retrieves the input text file's contents for a given [this@getRawInput].
+ * @receiver The puzzle metadata, consisting of [PuzzleMeta.year] and [PuzzleMeta.day].
+ * @param fileName The text file's name.
+ * @param resourceLoader The resource loader to be used to retrieve the input text file.
+ * @return The contents of the text file as a [String].
+ */
+fun PuzzleMeta.getRawInput(
+    fileName: String = "input.txt",
+    resourceLoader: ResourceLoader = ResourceLoader.Default
+) = getRawInput(
+    year = year.value, day = day, fileName = fileName, resourceLoader = resourceLoader
+)
 
 /**
  * Solves the specified puzzle.
