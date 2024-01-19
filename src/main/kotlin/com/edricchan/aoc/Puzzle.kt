38db410b844121342057aac0b6e9f6ecb5fc8453
Edric <edric.chan.1997@gmail.com>
2024-01-20 02:28:56 +0800

    Puzzle: Move input logic to sealed interface
    Instead of passing a custom input file name, the input logic
    is now customisable by using one of the possible sub-classes from the
    new `PuzzleInput` sealed interface:
    
    * `Raw`: raw text, does not require a file (to be used for tests)
    * `RawList`: variant of `Raw` which allows for a `List<String>` to be
      specified
    * `File`: path to an external `java.nio.file.Path` resource, to be
      loaded where needed
    * `ResourceFile`: load a resource via the Java resources (see
      `ResourceLoader`)
    * `Default` (name subject to change): the default implementation used for existing
      use-cases, to be used if no explicit input is specified
    
    The `PuzzleInput` interface has the following members that sub-classes
    must implement:
    * `lines()`: retrieve a list of lines from the input (e.g.
      `path.lines()`)
    * `raw()`: retrieve the raw text from the input (e.g. `path.readText()`)
    * `useLines`: similar to that of `Path.useLines` from Kotlin stdlib
    * `displayName`: the name to be used when printing the input type, to be
      added to the output log soon:tm:
    
    Currently, it's a sealed interface... I don't have any plans to make it
    a normal interface for now
    
    Additional changes:
    * Add extension properties/methods for retrieving certain types as
      `PuzzleInput` (e.g. `"123".input` -> `PuzzleInput.Raw("123")`)

--- src/main/kotlin/com/edricchan/aoc/Puzzle.kt
+++ src/main/kotlin/com/edricchan/aoc/Puzzle.kt
@@ -1,5 +1,6 @@
 package com.edricchan.aoc
 
+import com.edricchan.aoc.puzzle.input.PuzzleInput
 import java.time.LocalDate
 import java.time.Year
 
@@ -7,23 +8,58 @@ import java.time.Year
  * The puzzle question for a specific day.
  * @property year The specific year
  * @property day The specific day
- * @property customInput The custom input, if any. If specified, this value
- * takes precedence over the [input] property.
- * @property inputFileName The input's file name. By default, this value is set to
- * `"input.txt"`.
- * @property PartOneOutput The output type to be used for part 1
- * @property PartTwoOutput The output type to be used for part 2
+ * @property inputData The [PuzzleInput] type to use.
+ * If not specified, this value is set to [PuzzleInput.Default] with the [year] and
+ * [day] arguments specified respectively.
+ *
+ * To set the [PuzzleInput.Default.inputFileName] argument as well, use the overload
+ * of [Puzzle] which accepts an `inputFileName`.
+ * @param PartOneOutput The output type to be used for part 1
+ * @param PartTwoOutput The output type to be used for part 2
  */
 abstract class Puzzle<PartOneOutput, PartTwoOutput>(
     val year: Int = Year.now().value, val day: Int = LocalDate.now().dayOfMonth,
-    private val customInput: String? = null,
-    private val inputFileName: String = "input.txt"
+    val inputData: PuzzleInput = PuzzleInput.Default(Year.of(year), day),
 ) {
-    /** The input [File] for the specific puzzle. */
-    val inputFile by lazy { getInputFile(year, day, inputFileName) }
+    constructor(
+        year: Int,
+        day: Int,
+        // If this constructor is used, we can assume that we're explicitly passing
+        // an inputFileName, so there's no default value (to prevent overload
+        // resolution ambiguity).
+        inputFileName: String
+    ) : this(
+        year = year,
+        day = day,
+        inputData = PuzzleInput.Default(year, day, inputFileName)
+    )
+
+    @Deprecated(
+        "Use the constructor that takes a PuzzleInput instead",
+        ReplaceWith(
+            "Puzzle<PartOneOutput, PartTwoOutput>(" +
+                "year = year, day = day, customInput = PuzzleInput.Raw(it), inputFileName = inputFileName)",
+            "com.edricchan.aoc.puzzle.PuzzleInput"
+        )
+    )
+    constructor(
+        year: Int,
+        day: Int,
+        customInput: String?,
+        inputFileName: String = defaultInputFileName
+    ) : this(
+        year = year,
+        day = day,
+        inputData = customInput?.let { PuzzleInput.Raw(it) } ?: PuzzleInput.Default(
+            Year.of(year), day, inputFileName
+        ),
+    )
+
+    /** This puzzle's metadata. */
+    val meta = PuzzleMeta(Year.of(year), day)
 
     /** The input for the specific puzzle as a [List] of strings. */
-    val input by lazy { customInput?.lines() ?: getInput(year, day) }
+    val input: List<String> by lazy { inputData.lines() }
 
     /**
      * Allows for the input to be retrieved as a [Sequence] via [block],
@@ -32,16 +68,50 @@ abstract class Puzzle<PartOneOutput, PartTwoOutput>(
      * passed as a [Sequence].
      * @return The result as returned by executing [block].
      */
-    fun <T> useInput(block: (Sequence<String>) -> T) = useInput(year, day, block = block)
+    fun <T> useInput(block: (Sequence<String>) -> T): T = inputData.useLines(block)
 
-    /** The input for the specific puzzle as a [String]. */
-    val rawInput by lazy { customInput ?: getRawInput(year, day) }
+    /**
+     * Allows for the input to be retrieved as a [Sequence] via [block],
+     * closing the file once done processing.
+     *
+     * Variant of [useInput] which attempts to remove all empty lines.
+     * (For example, the end-of-file trailing new-line which might be added by
+     * default with tools like [EditorConfig](https://editorconfig.org).)
+     *
+     * This method is equivalent to the following code:
+     * ```kotlin
+     * useInput { lines ->
+     *     block(lines.filter { it.isNotBlank() })
+     * }
+     * ```
+     * @param block The callback to execute, where the contents of the file are
+     * passed as a [Sequence].
+     * @return The result as returned by executing [block].
+     * @see useInput
+     * @see Sequence.filter
+     * @see String.isNotBlank
+     */
+    fun <T> useNotBlankInput(block: (Sequence<String>) -> T): T = useInput { lines ->
+        block(lines.filter { it.isNotBlank() })
+    }
+
+    /**
+     * The input for the specific puzzle as a [String].
+     *
+     * To get the input split by line-endings, use [input] or [useInput] instead.
+     */
+    val rawInput: String by lazy { inputData.raw() }
 
     /** Retrieves the solution for part 1 of type [PartOneOutput]. */
     abstract fun solvePartOne(): PartOneOutput
 
     /** Retrieves the solution for part 2 of type [PartTwoOutput]. */
     abstract fun solvePartTwo(): PartTwoOutput
+
+    companion object {
+        /** The default input file name if not specified. */
+        const val defaultInputFileName = "input.txt"
+    }
 }
 
 /** Metadata for a [Puzzle]. This should be implemented by classes that extend from [Puzzle]. */
@@ -51,3 +121,40 @@ data class PuzzleMeta(
 ) {
     constructor(year: Int, day: Int) : this(Year.of(year), day)
 }
+
+/**
+ * Retrieves the default [PuzzleInput] from the receiver [PuzzleMeta].
+ * @param inputFileName The input's file name to be used.
+ * @see PuzzleInput.Default
+ */
+fun PuzzleMeta.getDefaultInput(
+    inputFileName: String = "input.txt"
+): PuzzleInput.Default = PuzzleInput.Default(meta = this, inputFileName = inputFileName)
+
+/**
+ * Retrieves the default [PuzzleInput] from the receiver [PuzzleMeta] using the
+ * default `input.txt` file name.
+ * @see PuzzleInput.Default
+ * @see PuzzleMeta.getDefaultInput
+ */
+val PuzzleMeta.defaultInput get() = getDefaultInput()
+
+/**
+ * Retrieves the default [PuzzleInput] from the receiver [Puzzle].
+ * @param inputFileName The input's file name to be used.
+ * @see PuzzleInput.Default
+ */
+fun Puzzle<*, *>.getDefaultInput(
+    inputFileName: String = "input.txt"
+): PuzzleInput.Default = PuzzleMeta(
+    year = year,
+    day = day
+).getDefaultInput(inputFileName)
+
+/**
+ * Retrieves the default [PuzzleInput] from the receiver [Puzzle] using the default
+ * `input.txt` file name.
+ * @see PuzzleInput.Default
+ * @see Puzzle.getDefaultInput
+ */
+val Puzzle<*, *>.defaultInput: PuzzleInput.Default get() = getDefaultInput()
---
+++ src/main/kotlin/com/edricchan/aoc/puzzle/input/PuzzleInput.kt
@@ -0,0 +1,214 @@
+package com.edricchan.aoc.puzzle.input
+
+import com.edricchan.aoc.Puzzle
+import com.edricchan.aoc.PuzzleMeta
+import com.edricchan.aoc.ResourceLoader
+import com.edricchan.aoc.getInputPath
+import com.edricchan.aoc.puzzle.input.PuzzleInput.RawList.Companion.lineSeparator
+import java.nio.file.Path
+import java.time.Year
+import kotlin.io.path.readLines
+import kotlin.io.path.readText
+import kotlin.io.path.useLines
+
+/**
+ * Represents input data for a [Puzzle].
+ *
+ * Its implementations are:
+ * * [PuzzleInput.Raw] - puzzle input which holds raw text
+ * * [PuzzleInput.RawList] - puzzle input which holds raw text as a list of strings
+ * * [PuzzleInput.File] - puzzle input from the specified [Path]
+ * * [PuzzleInput.ResourceFile] - puzzle input from Java resources, see [ResourceLoader]
+ * * [PuzzleInput.Default] - the default implementation if no explicit puzzle input is
+ * specified (and for backwards compatibility)
+ */
+sealed interface PuzzleInput {
+    /**
+     * Retrieves the list of lines from the given input.
+     * @see CharSequence.lines
+     * @see Path.readLines
+     */
+    fun lines(): List<String>
+
+    /**
+     * Retrieves the raw text from the given input.
+     * @see Path.readText
+     */
+    fun raw(): String
+
+    /**
+     * Calls the block callback giving it a sequence of all the lines in the input
+     * and optionally closes the reader once the processing is complete.
+     * @see CharSequence.lineSequence
+     * @see Path.useLines
+     */
+    fun <T> useLines(block: (Sequence<String>) -> T): T
+
+    /** The display name to use when printing this class. */
+    val displayName get() = toString()
+
+    /**
+     * An implementation of [PuzzleInput] that uses raw text.
+     * @property text The raw text.
+     */
+    @JvmInline
+    value class Raw(val text: String) : PuzzleInput {
+        override fun lines() = text.lines()
+
+        override fun raw() = text
+
+        override fun <T> useLines(block: (Sequence<String>) -> T) = block(text.lineSequence())
+    }
+
+    /**
+     * An implementation of [PuzzleInput] that uses raw text as a list of strings.
+     *
+     * ## Implementation notes
+     * * The [raw] method will [join][Iterable.joinToString] the [texts] items with the
+     * [`\n` line separator][lineSeparator].
+     * * The [lines] method will return the [texts] value _as-is_, without any
+     * additional processing (for example, removing line-separators).
+     * @property texts The raw text as a list of strings.
+     */
+    @JvmInline
+    value class RawList(val texts: List<String>) : PuzzleInput {
+        override fun lines() = texts
+
+        /** Retrieves the raw text from its input, joined by `\n`. */
+        override fun raw() = texts.joinToString(lineSeparator)
+
+        override fun <T> useLines(block: (Sequence<String>) -> T) = block(texts.asSequence())
+
+        companion object {
+            /** The line-separator character to use for [raw]. */
+            const val lineSeparator: String = "\n"
+        }
+    }
+
+    /**
+     * An implementation of [PuzzleInput] that uses a file [Path].
+     * @property path [Path] to the specified input file.
+     */
+    data class File(val path: Path) : PuzzleInput {
+        override fun lines() = path.readLines()
+
+        override fun raw() = path.readText()
+
+        override fun <T> useLines(block: (Sequence<String>) -> T) = path.useLines(block = block)
+
+        override val displayName: String
+            get() = "File - $path"
+    }
+
+    /**
+     * An implementation of [PuzzleInput] that uses a [ResourceLoader] with the given
+     * [resourceName].
+     * @property resourceName The resource name to retrieve.
+     * @property resourceLoader [ResourceLoader] used to retrieve the [resourceName].
+     * @see ResourceLoader.requireResourceAsPath
+     */
+    data class ResourceFile(
+        val resourceName: String,
+        val resourceLoader: ResourceLoader = ResourceLoader.Default
+    ) : PuzzleInput {
+        val path by lazy { resourceLoader.requireResourceAsPath(resourceName) }
+        override fun lines() = path.readLines()
+
+        override fun raw() = path.readText()
+
+        override fun <T> useLines(block: (Sequence<String>) -> T) = path.useLines(block = block)
+
+        override val displayName: String
+            get() = "ResourceFile (resource name: $resourceName) - $path"
+    }
+
+    /**
+     * The default implementation of [PuzzleInput] if not specified.
+     *
+     * The input data is read using the [resourceLoader] provided.
+     * @see getInputPath
+     */
+    data class Default(
+        val meta: PuzzleMeta,
+        val inputFileName: String = Puzzle.defaultInputFileName,
+        val resourceLoader: ResourceLoader = ResourceLoader.Default
+    ) : PuzzleInput {
+        constructor(
+            year: Year,
+            day: Int,
+            inputFileName: String = Puzzle.defaultInputFileName,
+            resourceLoader: ResourceLoader = ResourceLoader.Default
+        ) : this(
+            meta = PuzzleMeta(year, day),
+            inputFileName = inputFileName,
+            resourceLoader = resourceLoader
+        )
+
+        constructor(
+            year: Int,
+            day: Int,
+            inputFileName: String = Puzzle.defaultInputFileName,
+            resourceLoader: ResourceLoader = ResourceLoader.Default
+        ) : this(
+            year = Year.of(year),
+            day = day,
+            inputFileName = inputFileName,
+            resourceLoader = resourceLoader
+        )
+
+        /** Path to the input file. */
+        val path by lazy { meta.getInputPath(inputFileName) }
+
+        override fun lines() = path.readLines()
+
+        override fun raw() = path.readText()
+
+        override fun <T> useLines(block: (Sequence<String>) -> T) = path.useLines(block = block)
+
+        override val displayName: String
+            get() = "Default (metadata: $meta) - $path"
+    }
+}
+
+//#region Extension functions/properties
+/** Converts the receiver [String] to its [PuzzleInput.Raw] form. */
+fun String.toPuzzleInput(): PuzzleInput.Raw = PuzzleInput.Raw(this)
+
+/** Converts the receiver [String] to its [PuzzleInput.Raw] form. */
+val String.input: PuzzleInput.Raw get() = PuzzleInput.Raw(this)
+
+/** Converts the receiver [List] to its [PuzzleInput.RaRawListw] form. */
+fun List<String>.toPuzzleInput(): PuzzleInput.RawList = PuzzleInput.RawList(this)
+
+/** Converts the receiver [List] to its [PuzzleInput.RawList] form. */
+val List<String>.input: PuzzleInput.RawList get() = PuzzleInput.RawList(this)
+
+/** Converts the receiver [Path] to its [PuzzleInput.File] form. */
+fun Path.toPuzzleInput(): PuzzleInput.File = PuzzleInput.File(this)
+
+/** Converts the receiver [Path] to its [PuzzleInput.File] form. */
+val Path.input: PuzzleInput.File get() = PuzzleInput.File(this)
+
+/**
+ * Retrieves the [puzzle input][PuzzleInput.ResourceFile] given the specified arguments.
+ * @receiver The resource loader to resolve the [resourceName].
+ * @param resourceName The resource's name to resolve.
+ */
+fun ResourceLoader.getPuzzleInput(
+    resourceName: String
+): PuzzleInput.ResourceFile = PuzzleInput.ResourceFile(resourceName, this)
+
+/** Converts the receiver [PuzzleMeta] to its [PuzzleInput.Default] form. */
+val PuzzleMeta.input: PuzzleInput.Default get() = PuzzleInput.Default(this)
+
+/**
+ * Retrieves the [puzzle input][PuzzleInput.Default] given the specified arguments.
+ * @receiver The puzzle metadata.
+ * @param inputFileName The input's file name.
+ * @param resourceLoader Resource loader to resolve the [inputFileName].
+ */
+fun PuzzleMeta.getPuzzleInput(
+    inputFileName: String,
+    resourceLoader: ResourceLoader = ResourceLoader.Default
+): PuzzleInput.Default = PuzzleInput.Default(this, inputFileName, resourceLoader)
+//#endregion
